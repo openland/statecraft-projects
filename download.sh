00b92c7512e4042293cbbef33d7b68abe5e41493
Steve Kite <korshakov.stepan@gmail.com>
2018-01-02 14:13:06 -0800

    Loading Dates

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
-wget https://data.sfgov.org/api/views/ep85-j8df/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2013_Quarter_4.csv
+wget https://data.sfgov.org/api/views/ep85-j8df/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2013_Q4.csv
 
-wget https://data.sfgov.org/api/views/g383-7xmf/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2014_Quarter_1.csv
-wget https://data.sfgov.org/api/views/fv2q-qaux/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2014_Quarter_2.csv
-wget https://data.sfgov.org/api/views/n5ik-nmm3/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2014_Quarter_3.csv
-wget https://data.sfgov.org/api/views/858q-nwrm/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2014_Quarter_4.csv
+wget https://data.sfgov.org/api/views/g383-7xmf/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2014_Q1.csv
+wget https://data.sfgov.org/api/views/fv2q-qaux/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2014_Q2.csv
+wget https://data.sfgov.org/api/views/n5ik-nmm3/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2014_Q3.csv
+wget https://data.sfgov.org/api/views/858q-nwrm/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2014_Q4.csv
 
-wget https://data.sfgov.org/api/views/2cma-9y6y/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2015_Quarter_1.csv
-wget https://data.sfgov.org/api/views/w3e8-bxrm/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2015_Quarter_2.csv
-wget https://data.sfgov.org/api/views/apz9-dh7k/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2015_Quarter_3.csv
-wget https://data.sfgov.org/api/views/ra2x-jzmk/rows.csv?accessType=DOWNLOAD -O data/San_Francisco_Development_Pipeline_2015_Quarter_4.csv
+wget https://data.sfgov.org/api/views/2cma-9y6y/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2015_Q1.csv
+wget https://data.sfgov.org/api/views/w3e8-bxrm/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2015_Q2.csv
+wget https://data.sfgov.org/api/views/apz9-dh7k/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2015_Q3.csv
+wget https://data.sfgov.org/api/views/ra2x-jzmk/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2015_Q4.csv
 
 wget https://data.sfgov.org/api/views/dtz9-jkjt/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2016_Q1.csv
 wget https://data.sfgov.org/api/views/g5sr-9nhs/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2016_Q2.csv
@@ -16,3 +16,4 @@ wget https://data.sfgov.org/api/views/k7mk-w2pq/rows.csv?accessType=DOWNLOAD -O 
 wget https://data.sfgov.org/api/views/d7xx-7z6v/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2016_Q4.csv
 
 wget https://data.sfgov.org/api/views/7qzr-p6xn/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2017_Q1.csv
+wget https://data.sfgov.org/api/views/49cy-x5m5/rows.csv?accessType=DOWNLOAD -O data/SF_Development_Pipeline_2017_Q2.csv
